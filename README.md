f1433d2adf1f3f8a22fab2b70cc4458f4da3d820
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-06 22:54:36 +0200

    Add more benchmarks

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ![Contributors](https://img.shields.io/github/contributors/OkaeriPoland/okaeri-placeholders)
 [![Discord](https://img.shields.io/discord/589089838200913930)](https://discord.gg/hASN5eX)
 
-Simple blazing-fast placeholders library with yet unlimited possibilities. Part of the [okaeri-platform](https://github.com/OkaeriPoland/okaeri-platform).
+Simple blazing-fast placeholders library with yet unlimited capabilities. Part of the [okaeri-platform](https://github.com/OkaeriPoland/okaeri-platform).
 
 ## Installation
 ### Maven
@@ -49,3 +49,29 @@ PlaceholderContext context = PlaceholderContext.create()
 // process message and get output: Hola Mundo! ¿Cómo estás hoy? Estoy bien.
 String test = context.apply(message);
 ```
+
+## Benchmarks
+
+For the source code see [benchmarks](https://github.com/OkaeriPoland/okaeri-placeholders/tree/master/benchmarks) directory. 
+- Okaeri Placeholders benchmark is based on cached CompiledMessage as this is intended use of the library. PlaceholderContext is created every iteration.
+- JDK8 String#replace benchmarks are chaining replace calls together, as one may do while implementing this type of system with no additional code.
+- CommonsLang3 StringUtils#replaceEach benchmarks are using standard single call with no additional code.
+- System spec: (AdoptOpenJDK)(build 1.8.0_282-b08), OS: Ubuntu 20.04.2 LTS x86_64, CPU: AMD Ryzen 5 3600 (12) @ 3.600GHz
+
+### Simple
+This group represents average messages with fields.
+![](benchmark/results/1.0.0/simple.png)
+
+### Repeated
+This group uses multiline string built by repeating string from the simple group to check for repeated fields/stress performance.
+![](benchmark/results/1.0.0/repeated.png)
+
+### Long
+This group represents the replacement of a few evenly distributed fields inside a long string, e.g. html e-mail template. 
+The long tricky represents long string with one field in the last 20% of the string.
+![](benchmark/results/1.0.0/long.png)
+
+### Static
+This group shows differences in the performance for static strings that do not require any processing, but nor `String#replace` chain 
+  neither `StringUtils#replaceEach` can know that, causing performance loss when the character count is going up.
+![](benchmark/results/1.0.0/static.png)
--- benchmark/src/main/java/eu/okaeri/placeholders/benchmark/PlaceholdersBenchmark.java
+++ benchmark/src/main/java/eu/okaeri/placeholders/benchmark/PlaceholdersBenchmark.java
@@ -20,13 +20,23 @@ public class PlaceholdersBenchmark {
         // normal messages
         public CompiledMessage emptyMessage = CompiledMessage.of("");
         public CompiledMessage staticMessage = CompiledMessage.of("Hello World!");
+        public CompiledMessage staticRealMessage = CompiledMessage.of(StringUtils.repeat("Hello World! ", 10));
+        public CompiledMessage staticLongMessage = CompiledMessage.of(StringUtils.repeat("Hello World! ", 100));
         public CompiledMessage simpleMessage = CompiledMessage.of("Hello {who}! How are you {when}? I'm {how}.");
+        public CompiledMessage simpleLongerMessage = CompiledMessage.of("Hello {who}, it's nice to see you! How are you doing {when}? I'm {how}, but still recovering after writing that benchmark.");
 
         // example long message with just few placeholders
         public CompiledMessage longMessage;
         {
-            String gibberrish = StringUtils.repeat("abcd", 1000);
-            this.longMessage = CompiledMessage.of("Hello " + gibberrish + " {who}! How "  + gibberrish + " are you {when}? I'm " + gibberrish + " {how}." + gibberrish);
+            String gibberish = StringUtils.repeat("abcd", 1000);
+            this.longMessage = CompiledMessage.of("Hello " + gibberish + " {who}! How " + gibberish + " are you {when}? I'm " + gibberish + " {how}." + gibberish);
+        }
+
+        // example long message with just few placeholders
+        public CompiledMessage longTrickyMessage;
+        {
+            String gibberish = StringUtils.repeat("abcd", 1000);
+            this.longTrickyMessage = CompiledMessage.of("Hello " + gibberish + " world! How " + gibberish + " are you today? I'm " + gibberish + " {how}." + gibberish);
         }
 
         // example messages with repeating fields in varying amount
@@ -93,6 +103,60 @@ public class PlaceholdersBenchmark {
     }
     // endblock
 
+    // block static_real
+    @Benchmark
+    public void static_real_placeholders_contextwith(Blackhole blackhole, Data data) {
+        blackhole.consume(PlaceholderContext.create()
+                .with("who", data.field1)
+                .with("when", data.field2)
+                .with("how", data.field3)
+                .apply(data.staticRealMessage));
+    }
+
+    @Benchmark
+    public void static_real_java_replacechained(Blackhole blackhole, Data data) {
+        blackhole.consume(data.staticRealMessage.getRaw()
+                .replace("{who}", data.field1)
+                .replace("{when}", data.field2)
+                .replace("{how}", data.field3));
+    }
+
+    @Benchmark
+    public void static_real_commonslang3_replaceeach(Blackhole blackhole, Data data) {
+        blackhole.consume(StringUtils.replaceEach(data.staticRealMessage.getRaw(),
+                new String[]{"{who}", "{when}", "{how}"},
+                new String[]{data.field1, data.field2, data.field3}
+        ));
+    }
+    // endblock
+
+    // block static_long
+    @Benchmark
+    public void static_long_placeholders_contextwith(Blackhole blackhole, Data data) {
+        blackhole.consume(PlaceholderContext.create()
+                .with("who", data.field1)
+                .with("when", data.field2)
+                .with("how", data.field3)
+                .apply(data.staticLongMessage));
+    }
+
+    @Benchmark
+    public void static_long_java_replacechained(Blackhole blackhole, Data data) {
+        blackhole.consume(data.staticLongMessage.getRaw()
+                .replace("{who}", data.field1)
+                .replace("{when}", data.field2)
+                .replace("{how}", data.field3));
+    }
+
+    @Benchmark
+    public void static_long_commonslang3_replaceeach(Blackhole blackhole, Data data) {
+        blackhole.consume(StringUtils.replaceEach(data.staticLongMessage.getRaw(),
+                new String[]{"{who}", "{when}", "{how}"},
+                new String[]{data.field1, data.field2, data.field3}
+        ));
+    }
+    // endblock
+
     // block simple
     @Benchmark
     public void simple_placeholders_contextwith(Blackhole blackhole, Data data) {
@@ -114,6 +178,87 @@ public class PlaceholdersBenchmark {
     @Benchmark
     public void simple_commonslang3_replaceeach(Blackhole blackhole, Data data) {
         blackhole.consume(StringUtils.replaceEach(data.simpleMessage.getRaw(),
+                new String[]{"{who}", "{when}", "{how}"},
+                new String[]{data.field1, data.field2, data.field3}
+        ));
+    }
+    // endblock
+
+    // block simple_longer
+    @Benchmark
+    public void simple_longer_placeholders_contextwith(Blackhole blackhole, Data data) {
+        blackhole.consume(PlaceholderContext.create()
+                .with("who", data.field1)
+                .with("when", data.field2)
+                .with("how", data.field3)
+                .apply(data.simpleLongerMessage));
+    }
+
+    @Benchmark
+    public void simple_longer_java_replacechained(Blackhole blackhole, Data data) {
+        blackhole.consume(data.simpleLongerMessage.getRaw()
+                .replace("{who}", data.field1)
+                .replace("{when}", data.field2)
+                .replace("{how}", data.field3));
+    }
+
+    @Benchmark
+    public void simple_longer_commonslang3_replaceeach(Blackhole blackhole, Data data) {
+        blackhole.consume(StringUtils.replaceEach(data.simpleLongerMessage.getRaw(),
+                new String[]{"{who}", "{when}", "{how}"},
+                new String[]{data.field1, data.field2, data.field3}
+        ));
+    }
+    // endblock
+
+    // block long
+    @Benchmark
+    public void long_placeholders_contextwith(Blackhole blackhole, Data data) {
+        blackhole.consume(PlaceholderContext.create()
+                .with("who", data.field1)
+                .with("when", data.field2)
+                .with("how", data.field3)
+                .apply(data.longMessage));
+    }
+
+    @Benchmark
+    public void long_java_replacechained(Blackhole blackhole, Data data) {
+        blackhole.consume(data.longMessage.getRaw()
+                .replace("{who}", data.field1)
+                .replace("{when}", data.field2)
+                .replace("{how}", data.field3));
+    }
+
+    @Benchmark
+    public void long_commonslang3_replaceeach(Blackhole blackhole, Data data) {
+        blackhole.consume(StringUtils.replaceEach(data.longMessage.getRaw(),
+                new String[]{"{who}", "{when}", "{how}"},
+                new String[]{data.field1, data.field2, data.field3}
+        ));
+    }
+    // endblock
+
+    // block long_tricky
+    @Benchmark
+    public void long_tricky_placeholders_contextwith(Blackhole blackhole, Data data) {
+        blackhole.consume(PlaceholderContext.create()
+                .with("who", data.field1)
+                .with("when", data.field2)
+                .with("how", data.field3)
+                .apply(data.longTrickyMessage));
+    }
+
+    @Benchmark
+    public void long_tricky_java_replacechained(Blackhole blackhole, Data data) {
+        blackhole.consume(data.longTrickyMessage.getRaw()
+                .replace("{who}", data.field1)
+                .replace("{when}", data.field2)
+                .replace("{how}", data.field3));
+    }
+
+    @Benchmark
+    public void long_tricky_commonslang3_replaceeach(Blackhole blackhole, Data data) {
+        blackhole.consume(StringUtils.replaceEach(data.longTrickyMessage.getRaw(),
                 new String[]{"{who}", "{when}", "{how}"},
                 new String[]{data.field1, data.field2, data.field3}
         ));
