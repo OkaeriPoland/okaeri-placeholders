6ef53148a1e212880558ab04724a18e3125d8b4b
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-07 08:49:08 +0200

    Implement schema resolvers

--- src/main/java/eu/okaeri/placeholders/schema/resolver/DefaultSchemaResolver.java
+++ src/main/java/eu/okaeri/placeholders/schema/resolver/DefaultSchemaResolver.java
@@ -1,4 +1,35 @@
 package eu.okaeri.placeholders.schema.resolver;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class DefaultSchemaResolver implements SchemaResolver {
+
+    private static final Set<Class<?>> SUPPORTED_TOSTRING_TYPES = new HashSet<>(Arrays.asList(
+            BigDecimal.class,
+            BigInteger.class,
+            Boolean.class, boolean.class,
+            Byte.class, byte.class,
+            Character.class, char.class,
+            Double.class, double.class,
+            Float.class, float.class,
+            Integer.class, int.class,
+            Long.class, long.class,
+            Short.class, short.class,
+            String.class,
+            UUID.class));
+
+    @Override
+    public boolean supports(Class<?> type) {
+        return SUPPORTED_TOSTRING_TYPES.contains(type);
+    }
+
+    @Override
+    public String resolve(Object object) {
+        return object.toString();
+    }
 }
--- src/main/java/eu/okaeri/placeholders/schema/resolver/SchemaResolver.java
+++ src/main/java/eu/okaeri/placeholders/schema/resolver/SchemaResolver.java
@@ -1,4 +1,6 @@
 package eu.okaeri.placeholders.schema.resolver;
 
 public interface SchemaResolver {
+    boolean supports(Class<?> type);
+    String resolve(Object object);
 }
