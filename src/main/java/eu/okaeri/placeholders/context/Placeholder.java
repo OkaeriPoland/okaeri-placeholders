a7ac1ddd8c223d42f52cf1f43770d774b2d7a06a
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-07 08:16:52 +0200

    Optimize object creation

--- src/main/java/eu/okaeri/placeholders/context/Placeholder.java
+++ src/main/java/eu/okaeri/placeholders/context/Placeholder.java
@@ -8,7 +8,6 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -20,25 +19,22 @@ public class Placeholder {
     }
 
     private final Object value;
-    private final Map<MessageField, String> savedResults = new HashMap<>();
 
     @SuppressWarnings("unchecked")
     public String render(MessageField field) {
+        return render(this.value, field);
+    }
 
-        String savedResult = this.savedResults.get(field);
-        if (savedResult != null) {
-            return savedResult;
+    @SuppressWarnings("unchecked")
+    private static String render(Object object, MessageField field) {
+
+        if (object instanceof String) {
+            return (String) object;
         }
 
-        if (this.value instanceof String) {
-            String result = (String) this.value;
-            this.savedResults.put(field, result);
-            return result;
-        }
+        if (object instanceof PlaceholderSchema) {
 
-        if (this.value instanceof PlaceholderSchema) {
-
-            SchemaMeta meta = SchemaMeta.of((Class<? extends PlaceholderSchema>) this.value.getClass());
+            SchemaMeta meta = SchemaMeta.of((Class<? extends PlaceholderSchema>) object.getClass());
             if (!field.hasSub()) {
                 throw new RuntimeException("rendering PlaceholderSchema itself not supported at the moment");
             }
@@ -46,21 +42,16 @@ public class Placeholder {
             MessageField fieldSub = field.getSub();
             Map<String, PlaceholderResolver> placeholders = meta.getPlaceholders();
             PlaceholderResolver resolver = placeholders.get(fieldSub.getName());
+
             if (resolver == null) {
                 throw new RuntimeException("resolver cannot be null: " + fieldSub.getName());
             }
 
-            Object resolved = resolver.resolve(this.value);
-            Placeholder placeholder = Placeholder.of(resolved);
-            String result = placeholder.render(fieldSub);
-            this.savedResults.put(field, result);
-
-            return result;
+            Object resolved = resolver.resolve(object);
+            return render(resolved, fieldSub);
         }
 
         // FIXME: this fallback should be illegal lol
-        String result = String.valueOf(this.value);
-        this.savedResults.put(field, result);
-        return result;
+        return String.valueOf(object);
     }
 }
