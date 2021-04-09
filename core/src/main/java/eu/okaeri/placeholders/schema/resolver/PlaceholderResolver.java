16f4c0f0ec38d60539e801785fc9922a41dd3356
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-04-09 03:37:46 +0200

    Add more bukkit mappers, enum support

--- core/src/main/java/eu/okaeri/placeholders/schema/resolver/PlaceholderResolver.java
+++ core/src/main/java/eu/okaeri/placeholders/schema/resolver/PlaceholderResolver.java
@@ -1,5 +1,7 @@
 package eu.okaeri.placeholders.schema.resolver;
 
+// resolves object fields to objects
+// single element of the PlaceholderPack
 public interface PlaceholderResolver<T> {
     Object resolve(T from);
 }
--- core/src/main/java/eu/okaeri/placeholders/schema/resolver/SchemaResolver.java
+++ core/src/main/java/eu/okaeri/placeholders/schema/resolver/SchemaResolver.java
@@ -2,6 +2,7 @@ package eu.okaeri.placeholders.schema.resolver;
 
 import eu.okaeri.placeholders.message.part.MessageField;
 
+// resolves objects to strings
 public interface SchemaResolver {
     boolean supports(Class<?> type);
     String resolve(Object object, MessageField field);
