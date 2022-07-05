6138fe46d321732ebf78e0b305697387212e2e32
Sandra <sandra@okaeri.cloud>
2022-07-06 01:25:09 +0200

    Make MessageStatic#value mutable

--- core/src/main/java/eu/okaeri/placeholders/message/part/MessageStatic.java
+++ core/src/main/java/eu/okaeri/placeholders/message/part/MessageStatic.java
@@ -1,15 +1,12 @@
 package eu.okaeri.placeholders.message.part;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Data
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageStatic implements MessageElement {
 
-    private final String value;
+    private String value;
 
     public static MessageStatic of(@NonNull String value) {
         return new MessageStatic(value);
